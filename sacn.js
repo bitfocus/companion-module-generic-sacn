8ac8f80323b20ff1ac5d572dadad42b8e7ce2a37
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:30 +0000

    bulk replace system.emit calls

--- sacn.js
+++ sacn.js
@@ -196,7 +196,7 @@ instance.prototype.destroy = function() {
 
 instance.prototype.actions = function(system) {
 	var self = this;
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 
 		'setValue': {
 			label:'Set Value',
