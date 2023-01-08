19b441f475fe7a6b6d0c9ce3096885c28543c4b6
Julian Waller <git@julusian.co.uk>
2023-01-08 18:24:32 +0000

    chore: convert to es6 class

--- main.js
+++ main.js
@@ -1,421 +1,380 @@
-var sacnServer = require('./lib/server.js').server;
-var instance_skel = require('../../instance_skel');
-var log;
+var sacnServer = require('./lib/server.js').server
+var instance_skel = require('../../instance_skel')
 
+class SAcnInstance extends instance_skel {
+	constructor(system, id, config) {
+		super(system, id, config)
 
-function instance(system, id, config) {
-	var self = this;
-
-	// super-constructor
-	instance_skel.apply(this, arguments);
-
-	self.actions(); // export actions
-
-	return self;
-}
-
-instance.prototype.updateConfig = function(config) {
-	var self = this;
-
-	self.config = config;
-
-	self.init_sacn();
-};
-
-instance.prototype.init = function() {
-	var self = this;
-
-	debug = self.debug;
-	log = self.log;
-
-	self.status(self.STATE_UNKNOWN);
-
-	self.init_sacn();
-
-	self.init_feedbacks();
-};
-
-instance.prototype.keepAlive = function () {
-	var self = this;
-
-	if (self.server && self.packet) {
-		self.packet.setOption(self.packet.Options.TERMINATED, false);
-		self.server.send(self.packet);
+		this.actions() // export actions
 	}
-	if (self.server && !self.timer) {
-		self.timer = setInterval(function () {
-			if (self.server !== undefined) {
-				self.server.send(self.packet);
+
+	init() {
+		this.status(this.STATE_UNKNOWN)
+
+		this.init_sacn()
+
+		this.init_feedbacks()
+	}
+
+	updateConfig(config) {
+		this.config = config
+
+		this.init_sacn()
+	}
+
+	keepAlive() {
+		if (this.server && this.packet) {
+			this.packet.setOption(this.packet.Options.TERMINATED, false)
+			this.server.send(this.packet)
+		}
+		if (this.server && !this.timer) {
+			this.timer = setInterval(() => {
+				if (this.server !== undefined) {
+					this.server.send(this.packet)
+				}
+			}, 1000)
+		}
+		this.checkFeedbacks('status')
+	}
+	terminate() {
+		if (this.server !== undefined) {
+			this.packet.setOption(this.packet.Options.TERMINATED, true)
+			this.server.send(this.packet)
+		}
+		delete this.activeScene
+		if (this.timer) {
+			clearInterval(this.timer)
+			delete this.timer
+		}
+		this.checkFeedbacks('status')
+		this.checkFeedbacks('scene')
+	}
+	fade(steps, delay, offset, targets) {
+		if (steps) {
+			this.server.send(this.packet)
+			for (i = 0; i < targets.length; i++) {
+				var delta = targets[i + offset] - this.data[i + offset]
+				this.data[i + offset] += Math.round(delta / steps) & 0xff
 			}
-		}, 1000);
-	}
-	self.checkFeedbacks('status');
-};
-
-instance.prototype.terminate = function() {
-	var self = this;
-
-	if(self.server !== undefined) {
-		self.packet.setOption(self.packet.Options.TERMINATED, true);
-		self.server.send(self.packet);
-	}
-	delete self.activeScene;
-	if (self.timer) {
-		clearInterval(self.timer);
-		delete self.timer;
-	}
-	self.checkFeedbacks('status');
-	self.checkFeedbacks('scene');
-};	
-
-instance.prototype.fade = function(steps, delay, offset, targets) {
-	var self = this;
-
-	if ( steps ) {
-		self.server.send(self.packet);
-		for ( i=0; i < targets.length; i++ ) {
-			var delta = targets[i] - self.data[i+offset];
-			self.data[i+offset] += Math.round(delta/steps) & 0xff;
-		}
-		setTimeout(function() {self.fade(--steps, delay, offset, targets);}, delay);
-	} else {
-		for ( i=0; i < targets.length; i++ ) {
-			self.data[i+offset] = targets[i];
-		}
-		self.keepAlive();
-	}
-};
-
-instance.prototype.genUUID = function() {
-	// Crazy 1-liner UUIDv4 based on gist.github.com/jed/982883
-	// Consider just importing the UUID v4 module
-
-	function id(a) {return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,id)};
-
-	return id();
-};
-
-instance.prototype.init_sacn= function() {
-	var self = this;
-
-	self.status(self.STATE_UNKNOWN);
-
-	if(self.server !== undefined) {
-		self.terminate();
-		delete self.server;
-		delete self.packet;
-		delete self.data;
-	}
-
-	if(self.config.host) {
-		self.server = new sacnServer(self.config.host);
-		self.packet = self.server.createPacket(512);
-		self.data = self.packet.getSlots();
-
-		self.packet.setSourceName(self.config.name || "Companion App");
-		self.packet.setUUID(self.config.uuid || self.genUUID());
-		self.packet.setUniverse(self.config.universe || 0x01);
-		self.packet.setPriority(self.config.priority);
-		self.packet.setOption(self.packet.Options.TERMINATED, true);
-
-		for(var i=0; i<self.data.length; i++) {
-			self.data[i] = 0x00;
+			setTimeout(() => {
+				this.fade(--steps, delay, offset, targets)
+			}, delay)
+		} else {
+			for (i = 0; i < targets.length; i++) {
+				this.data[i + offset] = targets[i + offset]
+			}
+			this.keepAlive()
 		}
 	}
-
-	self.status(self.STATE_OK);
-};
-
-// Return config fields for web config
-instance.prototype.config_fields = function () {
-
-	var self = this;
-	var fields = [
-		{
-			type: 'text',
-			id: 'info',
-			width: 12,
-			label: 'Information',
-			value: 'This module will transmit SACN packets to the ip and universe you specify below. If you need more universes, add multiple SACN instances.'
-		},
-		{
-			type: 'textinput',
-			id: 'name',
-			width: 12,
-			label: 'Source Name',
-			default: 'Companion (' + self.id + ')'
-		},
-		{
-			type: 'textinput',
-			id: 'uuid',
-			width: 12,
-			label: 'Source UUID',
-			default: self.genUUID()
-		},
-		{
-			type: 'textinput',
-			id: 'host',
-			label: 'Receiver IP',
-			width: 5,
-			regex: self.REGEX_IP
-		},
-		{
-			type: 'number',
-			id: 'universe',
-			label: 'Universe (1-63999)',
-			width: 4,
-			min: 1,
-			max: 63999,
-			default: 1
-		},
-		{
-			type: 'number',
-			id: 'priority',
-			label: 'Priority (1-201)',
-			width: 3,
-			min: 1,
-			max: 201,
-			default: 100
-		}
-	];
-	return fields;
-};
-
-// When module gets deleted
-instance.prototype.destroy = function() {
-	var self = this;
-
-	if (self.server !== undefined) {
-		self.terminate();
-		delete self.server;
-		delete self.packet;
-		delete self.data;
-	}
-};
-
-
-instance.prototype.actions = function(system) {
-	var self = this;
-	self.setActions({
-
-		'setValue': {
-			label:'Set Value',
-			options: [
-				{
-					type: 'number',
-					label: 'Channel (1-512)',
-					id: 'channel',
-					min: 1,
-					max: 512,
-					default: 1
-				},
-				{
-					type: 'number',
-					label: 'Value (0-255)',
-					id: 'value',
-					min: 0,
-					max: 255,
-					default: 0
-				}
-			]
-		},
-		'setValues': {
-			label: 'Set Values',
-			options: [
-				{
-					type: 'number',
-					label: 'Starting Channel (1-512)',
-					id: 'start',
-					min: 1,
-					max: 512,
-					default: 1
-				},
-				{
-					type: 'textinput',
-					label: 'Values (space-separated list)',
-					id: 'values',
-					regex: '/((^| )([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){1,512}$/',
-					default: '0 1 255'
-				}
-			]
-		},
-		'fadeValues': {
-			label: 'Fade To Values',
-			options: [
-				{
-					type: 'number',
-					label: 'Fade steps',
-					id: 'steps',
-					min: 0,
-					max: 100,
-					default: 30
-				},
-				{
-					type: 'number',
-					label: 'Fade duration (ms)',
-					id: 'duration',
-					min: 30,
-					max: 10000,
-					default: 1000
-				},
-				{
-					type: 'number',
-					label: 'Starting Channel (1-512)',
-					id: 'start',
-					min: 1,
-					max: 512,
-					default: 1
-				},
-				{
-					type: 'textinput',
-					label: 'Values (space-separated list)',
-					id: 'values',
-					regex: '/((^| )([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){1,512}$/',
-					default: '0 1 255'
-				}
-			]
-		},
-		'setScene': {
-			label: 'Set Active Scene',
-			options: [
-				{
-					type: 'number',
-					label: 'Scene Number',
-					id: 'scene',
-					min: 0,
-					max: 1024
-				}
-			]
-		},
-		'terminate': {
-			label:'Terminate Stream'
+	genUUID() {
+		// Crazy 1-liner UUIDv4 based on gist.github.com/jed/982883
+		// Consider just importing the UUID v4 module
+		function id(a) {
+			return a
+				? (a ^ ((Math.random() * 16) >> (a / 4))).toString(16)
+				: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, id)
 		}
 
-	});
-}
-
-instance.prototype.action = function(action) {
-	var self = this;
-
-
-	switch (action.action) {
-
-		case 'setValue':
-			if (self.server !== undefined) {
-				self.data[action.options.channel-1] = action.options.value;
-				self.keepAlive();
-			}
-			break;
-		case 'setValues':
-			if (self.server !== undefined) {
-				var values = action.options.values.split(' ');
-				for (i=0; i < values.length; i++) {
-					self.data[i + (action.options.start - 1)] = values[i];
-				}
-				self.keepAlive();
-			}
-			break;
-		case 'fadeValues':
-			if (self.server !== undefined) {
-				var opts = action.options;
-				var values = opts.values.split(' ');
-
-				self.fade(opts.steps, Math.floor(opts.duration/opts.steps), (opts.start - 1), values);
-			}
-			break;
-		case 'setScene':
-			self.activeScene = action.options.scene;
-			self.checkFeedbacks('scene');
-			break;
-		case 'terminate':
-			if (self.server !== undefined) {
-				self.terminate();
-			}
-			break;
-
+		return id()
 	}
+	init_sacn() {
+		this.status(this.STATE_UNKNOWN)
 
-}
+		if (this.server !== undefined) {
+			this.terminate()
+			delete this.server
+			delete this.packet
+			delete this.data
+		}
 
-instance.prototype.init_feedbacks = function() {
-	var self = this;
-	const feedbacks = {}
-	
-	feedbacks['status'] = {
-		label: 'Stream status',
-		description: 'Set the button background based on the status of the sACN stream.',
-		options: [
+		if (this.config.host) {
+			this.server = new sacnServer(this.config.host)
+			this.packet = this.server.createPacket(512)
+			this.data = this.packet.getSlots()
+
+			this.packet.setSourceName(this.config.name || 'Companion App')
+			this.packet.setUUID(this.config.uuid || this.genUUID())
+			this.packet.setUniverse(this.config.universe || 0x01)
+			this.packet.setPriority(this.config.priority)
+			this.packet.setOption(this.packet.Options.TERMINATED, true)
+
+			for (var i = 0; i < this.data.length; i++) {
+				this.data[i] = 0x00
+			}
+		}
+
+		this.status(this.STATE_OK)
+	}
+	// Return config fields for web config
+	config_fields() {
+		var fields = [
 			{
-				type: 'colorpicker',
-				label: 'Background color',
-				id: 'bg_color',
-				default: this.rgb(0, 0, 0)
+				type: 'text',
+				id: 'info',
+				width: 12,
+				label: 'Information',
+				value:
+					'This module will transmit SACN packets to the ip and universe you specify below. If you need more universes, add multiple SACN instances.',
 			},
 			{
-				type: 'colorpicker',
-				label: 'Foreground color',
-				id: 'fg_color',
-				default: this.rgb(255,255,255)
+				type: 'textinput',
+				id: 'name',
+				width: 12,
+				label: 'Source Name',
+				default: 'Companion (' + this.id + ')',
 			},
 			{
-				type: 'dropdown',
-				label: 'Status',
-				id: 'status',
-				default: 1,
-				choices: [ {id: '1', label: 'Active'}, {id: '0', label: 'Terminated'} ]
-			}
-		]
-	}
-	feedbacks['scene'] = {
-		label: 'Active scene',
-		description: 'Set the button colors based on the active scene.',
-		options: [
-			{
-				type: 'colorpicker',
-				label: 'Background color',
-				id: 'bg_color',
-				default: this.rgb(0, 20, 208)
+				type: 'textinput',
+				id: 'uuid',
+				width: 12,
+				label: 'Source UUID',
+				default: this.genUUID(),
 			},
 			{
-				type: 'colorpicker',
-				label: 'Foreground color',
-				id: 'fg_color',
-				default: this.rgb(255, 255, 255)
+				type: 'textinput',
+				id: 'host',
+				label: 'Receiver IP',
+				width: 5,
+				regex: this.REGEX_IP,
 			},
 			{
 				type: 'number',
-				label: 'Scene',
-				id: 'scene',
-				min: 0,
-				max: 1024
-			}
+				id: 'universe',
+				label: 'Universe (1-63999)',
+				width: 4,
+				min: 1,
+				max: 63999,
+				default: 1,
+			},
+			{
+				type: 'number',
+				id: 'priority',
+				label: 'Priority (1-201)',
+				width: 3,
+				min: 1,
+				max: 201,
+				default: 100,
+			},
 		]
+		return fields
 	}
-	self.setFeedbackDefinitions(feedbacks);
-};
+	// When module gets deleted
+	destroy() {
+		if (this.server !== undefined) {
+			this.terminate()
+			delete this.server
+			delete this.packet
+			delete this.data
+		}
+	}
+	actions() {
+		this.setActions({
+			setValue: {
+				label: 'Set Value',
+				options: [
+					{
+						type: 'number',
+						label: 'Channel (1-512)',
+						id: 'channel',
+						min: 1,
+						max: 512,
+						default: 1,
+					},
+					{
+						type: 'number',
+						label: 'Value (0-255)',
+						id: 'value',
+						min: 0,
+						max: 255,
+						default: 0,
+					},
+				],
+			},
+			setValues: {
+				label: 'Set Values',
+				options: [
+					{
+						type: 'number',
+						label: 'Starting Channel (1-512)',
+						id: 'start',
+						min: 1,
+						max: 512,
+						default: 1,
+					},
+					{
+						type: 'textinput',
+						label: 'Values (space-separated list)',
+						id: 'values',
+						regex: '/((^| )([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){1,512}$/',
+						default: '0 1 255',
+					},
+				],
+			},
+			fadeValues: {
+				label: 'Fade To Values',
+				options: [
+					{
+						type: 'number',
+						label: 'Fade steps',
+						id: 'steps',
+						min: 0,
+						max: 100,
+						default: 30,
+					},
+					{
+						type: 'number',
+						label: 'Fade duration (ms)',
+						id: 'duration',
+						min: 30,
+						max: 10000,
+						default: 1000,
+					},
+					{
+						type: 'number',
+						label: 'Starting Channel (1-512)',
+						id: 'start',
+						min: 1,
+						max: 512,
+						default: 1,
+					},
+					{
+						type: 'textinput',
+						label: 'Values (space-separated list)',
+						id: 'values',
+						regex: '/((^| )([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])){1,512}$/',
+						default: '0 1 255',
+					},
+				],
+			},
+			setScene: {
+				label: 'Set Active Scene',
+				options: [
+					{
+						type: 'number',
+						label: 'Scene Number',
+						id: 'scene',
+						min: 0,
+						max: 1024,
+					},
+				],
+			},
+			terminate: {
+				label: 'Terminate Stream',
+			},
+		})
+	}
+	action(action) {
+		switch (action.action) {
+			case 'setValue':
+				if (this.server !== undefined) {
+					this.data[action.options.channel - 1] = action.options.value
+					this.keepAlive()
+				}
+				break
+			case 'setValues':
+				if (this.server !== undefined) {
+					var values = action.options.values.split(' ')
+					for (i = 0; i < values.length; i++) {
+						this.data[i + (action.options.start - 1)] = values[i]
+					}
+					this.keepAlive()
+				}
+				break
+			case 'fadeValues':
+				if (this.server !== undefined) {
+					var opts = action.options
+					var values = opts.values.split(' ')
 
-instance.prototype.feedback = function(feedback) {
-	var self = this;
-	var opts = feedback.options;
-	
-	if (self.packet === undefined) {
-		return {};
-	}
-	else if (feedback.type == 'status') {
-		if (opts.status == 0 && self.packet.getOption(self.packet.Options.TERMINATED)) {
-			return { bgcolor: opts.bg_color, color: opts.fg_color }
-		}
-		else if (opts.status == 1 && !self.packet.getOption(self.packet.Options.TERMINATED)) {
-			return { bgcolor: opts.bg_color, color: opts.fg_color }
-		}
-	}
-	else if (feedback.type == 'scene') {
-		if (self.activeScene && opts.scene == self.activeScene) {
-			return { bgcolor: opts.bg_color, color: opts.fg_color }
+					this.fade(opts.steps, Math.floor(opts.duration / opts.steps), opts.start - 1, values)
+				}
+				break
+			case 'setScene':
+				this.activeScene = action.options.scene
+				this.checkFeedbacks('scene')
+				break
+			case 'terminate':
+				if (this.server !== undefined) {
+					this.terminate()
+				}
+				break
 		}
 	}
-	return {}
-};
+	init_feedbacks() {
+		const feedbacks = {}
 
-instance_skel.extendedBy(instance);
-exports = module.exports = instance;
+		feedbacks['status'] = {
+			label: 'Stream status',
+			description: 'Set the button background based on the status of the sACN stream.',
+			options: [
+				{
+					type: 'colorpicker',
+					label: 'Background color',
+					id: 'bg_color',
+					default: this.rgb(0, 0, 0),
+				},
+				{
+					type: 'colorpicker',
+					label: 'Foreground color',
+					id: 'fg_color',
+					default: this.rgb(255, 255, 255),
+				},
+				{
+					type: 'dropdown',
+					label: 'Status',
+					id: 'status',
+					default: 1,
+					choices: [
+						{ id: '1', label: 'Active' },
+						{ id: '0', label: 'Terminated' },
+					],
+				},
+			],
+		}
+		feedbacks['scene'] = {
+			label: 'Active scene',
+			description: 'Set the button colors based on the active scene.',
+			options: [
+				{
+					type: 'colorpicker',
+					label: 'Background color',
+					id: 'bg_color',
+					default: this.rgb(0, 20, 208),
+				},
+				{
+					type: 'colorpicker',
+					label: 'Foreground color',
+					id: 'fg_color',
+					default: this.rgb(255, 255, 255),
+				},
+				{
+					type: 'number',
+					label: 'Scene',
+					id: 'scene',
+					min: 0,
+					max: 1024,
+				},
+			],
+		}
+		this.setFeedbackDefinitions(feedbacks)
+	}
+	feedback(feedback) {
+		var opts = feedback.options
+
+		if (this.packet === undefined) {
+			return {}
+		} else if (feedback.type == 'status') {
+			if (opts.status == 0 && this.packet.getOption(this.packet.Options.TERMINATED)) {
+				return { bgcolor: opts.bg_color, color: opts.fg_color }
+			} else if (opts.status == 1 && !this.packet.getOption(this.packet.Options.TERMINATED)) {
+				return { bgcolor: opts.bg_color, color: opts.fg_color }
+			}
+		} else if (feedback.type == 'scene') {
+			if (this.activeScene && opts.scene == this.activeScene) {
+				return { bgcolor: opts.bg_color, color: opts.fg_color }
+			}
+		}
+		return {}
+	}
+}
+
+exports = module.exports = SAcnInstance
